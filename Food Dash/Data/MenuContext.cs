78e0be7bd6aaa212dccd06f97dd4772c0dd7357d
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-05-10 16:36:43 +0700

    Added OnModelCreating()

--- Food Dash/Data/MenuContext.cs
+++ Food Dash/Data/MenuContext.cs
@@ -6,5 +6,14 @@ namespace Food_Dash.Data
     public class MenuContext : DbContext
     {
         public DbSet<Menu> Menus { get; set; }
+
+        public MenuContext(DbContextOptions<MenuContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Menu>().ToTable("Menu");
+        }
     }
 }
