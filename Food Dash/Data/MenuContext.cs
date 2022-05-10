86638be49db899686bed562bf0aed62bc756ae8e
Aradhana Indra D(ev) <aradhanaindradev@gmail.com>
2022-05-10 16:33:52 +0700

    Added framework tools with the appropriate version

---
+++ Food Dash/Data/MenuContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Food_Dash.Models;
+
+namespace Food_Dash.Data
+{
+    public class MenuContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
